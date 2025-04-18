7ccfa074d2d4d417d9f18bb21a40945cebd0aa97
Choohoeun <hoeun8793@gachon.ac.kr>
2025-04-18 10:31:02 +0900

    Add login page component

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,11 @@
+import LoginCard from '../components/login/LoginCard';
+import '../styles/Login.css';
+import React from 'react';
+
+export default function LoginPage() {
+  return (
+    <div className="login-page-wrapper">
+      <LoginCard />
+    </div>
+  );
+}
